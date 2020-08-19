a20f0ee05d8f589a045d899d6f700e36de60dc53
ramdon_person <48343703+ramdon-person@users.noreply.github.com>
2020-08-19 10:42:37 -0700

    Update InteractEvent.java

--- src/com/voidcitymc/plugins/SimpleBankRobbing/InteractEvent.java
+++ src/com/voidcitymc/plugins/SimpleBankRobbing/InteractEvent.java
@@ -4,6 +4,7 @@ import com.voidcitymc.api.SimplePolice.SimplePoliceAPI;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
